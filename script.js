7b599eabecabec02c3be1fa05ba07ab429e6a136
AkhatorKing1 <aenoseta@gmail.com>
2022-01-18 10:33:13 +0100

    commit progress

--- script.js
+++ script.js
@@ -3,20 +3,27 @@
     const close = document.querySelector('.close')
     const nav = document.querySelector('nav')
     const blurDiv = document.querySelector('.blur-div')
+    const container = document.querySelector('.container')
+    const homeExplore = document.querySelector('.home-explore')
 
     $(window).resize(function() {
         if ($(window).width() >=768) {
             location.reload();
          }
      });
-     
-
+    
+    let displaySwitch = "OFF"
     function displayNav() {
         if(nav.style.display === "block") {
             nav.style.display = "none";
+            displaySwitch = "OFF"
+            console.log(displaySwitch)
+
         }
         else {
             nav.style.display = "block";
+            displaySwitch = "ON"
+            console.log(displaySwitch)
         }
     }
 
@@ -29,17 +36,54 @@
         }
     }
 
+    const newDiv = document.createElement("div")
+    $(newDiv).css({"background": "transparent", "width": "100vw", "height": "100vh", "position": "fixed", "opacity": "0.1"})
+
     hamburger.addEventListener('click', () => {
         displayNav();
         displayBlurDiv();
+        container.insertBefore(newDiv, container.childNodes[0])
         hamburger.style.display = "none"
     })
 
     close.addEventListener('click', () => {
         displayNav();
         displayBlurDiv();
+        container.removeChild(newDiv);
         hamburger.style.display = "flex"
     })
+
+    // function removeNav() {
+    //     if ($(window).width() <768  && nav.display === "block") {
+    //         container.addEventListener('click', () => {
+    //             nav.style.display = "none"
+    //             console.log("clicked")
+    //         })
+    //      }
+    // }
+
+    function removeNav() {
+        newDiv.addEventListener('click', () => {
+            if ($(window).width() <768  && displaySwitch === "ON") {
+                nav.style.display = "none"
+                blurDiv.style.display = "none";
+                hamburger.style.display = "flex"
+                container.removeChild(newDiv)
+                displaySwitch = "OFF"
+                console.log("clicked")
+            }
+
+            // homeExplore.addEventListener('click', () => {
+            //     if ($(window).width() <768  && displaySwitch === "ON") {
+            //         nav.style.display = "none"
+            //         console.log("clicked again")
+            //     }
+            // })
+        })
+        
+    }
+
+    removeNav()
 
     const newNav = document.querySelector('.new-nav')
     const memberPosition = document.querySelector('#member-position')
@@ -76,21 +120,33 @@
         }
     
         $(window).resize(function() {
-            if ($(window).width() >768 && $(window).width() <992) {
+            if ($(window).width() >=768 && $(window).width() <992) {
                 moveDown()
             }
             });
     
-        if ($(window).width() >768 && $(window).width() <992) {
+        if ($(window).width() >=768 && $(window).width() <992) {
             moveDown()
         }
     } else (console.log('no .tech-type'))
-    
 
-     const vehicle = document.querySelector('.vehicle')
-     const spaceport = document.querySelector('.spaceport')
-     const capsule = document.querySelector('.capsule')
-     const vehicleImg = document.querySelector('#vehicle-img')
-     const spaceportImg = document.querySelector('#spaceport-img')
-     const capsuleImg = document.querySelector('#capsule-img')
+    function blurMargin() {
+        $(window).resize(function() {
+            if ($(window).width() >=768) {
+                $(blurDiv).css({"margin-left": "0"})
+            }
+            else {
+                $(blurDiv).css({"margin-left": "calc(100% - 254px)"})
+            }
+            });
+    
+        if ($(window).width() >=768) {
+            $(blurDiv).css({"margin-left": "0"})
+        }
+        else {
+            $(blurDiv).css({"margin-left": "calc(100% - 254px)"})
+        }
+    }
+
+    blurMargin()
 })()
